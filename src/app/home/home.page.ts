919db867274c18abd095f2ab8d816ff49188dc72
mifffth <miftahdesmasyahputra@mail.ugm.ac.id>
2024-09-26 13:55:37 +0700

    Change to using leaflet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import * as L from 'leaflet';
 
 // Fix for missing default Leaflet marker icons
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-});
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+//   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+//   shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+// });
 
 @Component({
   selector: 'app-home',
@@ -17,36 +17,50 @@ export class HomePage {
   map!: L.Map;
   tileLayers: any = {};
   currentLayer!: L.TileLayer;
+  currentMarker: L.Marker | null = null; // Store the current marker
 
   constructor() {}
   ngOnInit() {}
 
   ionViewDidEnter() {
-    // Initialize map
-    this.map = L.map('mapId').setView([51.505, -0.09], 13);
+    this.map = L.map('mapId').setView([-7.797068, 110.370529], 13); // Set peta di Yogyakarta
 
-    // Define basemap layers
-    this.tileLayers = {
-      topo: L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }),  
-      streets: L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }),  
-      hybrid: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, SRTM | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (CC-BY-SA)' }),
-    };
+    // OSM Base map
+    const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-    // Set default basemap (streets)
-    this.currentLayer = this.tileLayers.streets;
-    this.currentLayer.addTo(this.map);
+    // Satellite Base map
+    const satellite = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-    // Add blue popup marker on map click
+    // Terrain Base map
+    const terrain = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, SRTM | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (CC-BY-SA)'
+    });
+
+    osm.addTo(this.map); // Default basemap
+
+    // Menambahkan layer control untuk memilih basemap
+    L.control.layers({
+      "OpenStreetMap": osm,
+      "Satellite": satellite,
+      "Terrain": terrain
+    }).addTo(this.map);
+
     this.map.on('click', (e: any) => {
       const { lat, lng } = e.latlng;
-      L.marker([lat, lng])
+
+      // Remove the previous marker if it exists
+      if (this.currentMarker) {
+        this.map.removeLayer(this.currentMarker);
+      }
+
+      // Add a new marker and store its reference
+      this.currentMarker = L.marker([lat, lng])
         .addTo(this.map)
-        .bindPopup(`<b>Location:</b> ${lat.toFixed(5)}, ${lng.toFixed(5)}`)
+        .bindPopup(`<b>You are in:</b> ${lat.toFixed(5)}, ${lng.toFixed(5)}`)
         .openPopup();
     });
   }
